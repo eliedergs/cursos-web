6ede0ab05d3441f71820a7bd109903941a2b7cd9
Elieder Gomes <elieder93@gmail.com>
2022-12-16 16:38:58 -0300

    feat: adicionar toast de mensagens

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,14 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 
 import CoursesDetailsPage from '@/pages/course-details';
 import CousesPage from '@/pages/courses';
+import { Toaster } from 'react-hot-toast';
 
 const Main = () => {
     return (
         <Container>
-            <Routes>
-                <Route path="/:id" element={<CoursesDetailsPage />} />
-                <Route path="/" element={<CousesPage />} />
-            </Routes>
+            <>
+                <Toaster />
+                <Routes>
+                    <Route path="/:id" element={<CoursesDetailsPage />} />
+                    <Route path="/" element={<CousesPage />} />
+                </Routes>
+            </>
         </Container>
     );
 };
